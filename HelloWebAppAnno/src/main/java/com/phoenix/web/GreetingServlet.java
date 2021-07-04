9e0c3ad9470ad5502829671ed07ed034a84b6237
Md Faisal <cs.mdfaisal@gmail.com>
2021-07-04 13:51:58 +0530

    Annotation

---
+++ HelloWebAppAnno/src/main/java/com/phoenix/web/GreetingServlet.java
@@ -0,0 +1,46 @@
+package com.phoenix.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GreetingServlet
+ */
+@WebServlet("/greet")
+public class GreetingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init method");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy method");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("service method");
+		PrintWriter out=response.getWriter();
+		out.println("<h1> welcome Annotation observation to dynamic world of java web application</h1>");
+	}
+
+}
